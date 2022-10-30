9699f9b1aad9f1feafcfbaf05cfb33647f47e210
Roxas2402 <98048769+Roxas2402@users.noreply.github.com>
2022-10-30 14:48:41 +0100

    aa

--- app/src/main/java/com/example/repaso_examen/MainActivity.java
+++ app/src/main/java/com/example/repaso_examen/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.repaso_examen.databinding.ActivityMainBinding;
@@ -25,6 +26,8 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO: CREAR EDITAR INMUEBLE
 
     //1: Creamos la clase Inmueble
     //1.01: Creamos la clase constantes
@@ -45,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        //TODO: DESPUÉS DE HACER EL ADDINMUEBLE HAY QUE CONFIGURAR ESTO Y PONERLE NÚMERO
         //10: Crear la actividad en la que se muestra el inmueble creado (inmueble_view_model.xml) y borramos la clase que se crea
         //11: Inicializamos el launcher
         inicializaLaunchers();
@@ -66,19 +68,35 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == RESULT_OK) {
-                            if (result.getData().getExtras().getSerializable(Constants.INMUEBLE) != null) {
-                                Inmueble inmueble = (Inmueble) result.getData().getExtras().getSerializable(Constants.INMUEBLE);
-                                inmueblesList.add(inmueble);
-                                //12: Cuando le demos a crear, se mostrará el inmueble
-                                muestraInmuebleContenido();
+                            if (result.getData() != null) {
+                                if (result.getData().getExtras() != null) {
+                                    if (result.getData().getExtras().getSerializable(Constants.INMUEBLE) != null) {
+                                        //Extraigo el inmueble
+                                        Inmueble inmueble = (Inmueble) result.getData().getExtras().getSerializable(Constants.INMUEBLE);
+                                        //Agrego el inmueble
+                                        inmueblesList.add(inmueble);
+                                        //Muestro los inmuebles
+                                        //12: Mostrará los datos que hay en el bundle
+                                        muestraInmueblesContenido();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "NO HAY DATOS", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(MainActivity.this, "NO HAY BUNDLE EN EL INTENT", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, "NO HAY INTENT", Toast.LENGTH_SHORT).show();
                             }
+                        } else {
+                            Toast.makeText(MainActivity.this, "VENTANA CANCELADA", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
+
         );
     }
 
-    private void muestraInmuebleContenido() {
+    private void muestraInmueblesContenido() {
         //12.01: Creamos el ScrollView en el content_main.xml y renombramos la cosa esa rara en el activity_main.xml
         binding.contentMain.contenedor.removeAllViews();
 
